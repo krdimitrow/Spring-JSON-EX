90b2793d67235f743f53e7ef9b5670808956be6e
Kristiyan Dimitrov <krisoz142@gmail.com>
2021-07-20 16:05:23 +0300

    Initial commit

---
+++ src/main/java/com/example/jsonex/constant/GlobalConstant.java
@@ -0,0 +1,6 @@
+package com.example.jsonex.constant;
+
+public class GlobalConstant {
+
+    public static final String RESOURCES_FILE_PATH = "src/main/resources/files/";
+}
---
+++ src/main/java/com/example/jsonex/service/impl/ProductServiceImpl.java
@@ -0,0 +1,88 @@
+package com.example.jsonex.service.impl;
+
+import com.example.jsonex.model.dto.ProductNameAndPriceDto;
+import com.example.jsonex.model.dto.ProductSeedDto;
+import com.example.jsonex.model.entity.Product;
+import com.example.jsonex.repository.ProductRepository;
+import com.example.jsonex.service.CategoryService;
+import com.example.jsonex.service.ProductService;
+import com.example.jsonex.service.UserService;
+import com.example.jsonex.util.ValidationUtil;
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.example.jsonex.constant.GlobalConstant.RESOURCES_FILE_PATH;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private static final String PRODUCTS_FILE_NAME = "products.json";
+
+    private final ProductRepository productRepository;
+    private final UserService userService;
+    private final CategoryService categoryService;
+    private final ValidationUtil validationUtil;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+
+    public ProductServiceImpl(ProductRepository productRepository, UserService userService, CategoryService categoryService, ValidationUtil validationUtil, ModelMapper modelMapper, Gson gson) {
+        this.productRepository = productRepository;
+        this.userService = userService;
+        this.categoryService = categoryService;
+        this.validationUtil = validationUtil;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+    }
+
+
+    @Override
+    public void seedProduct() throws IOException {
+        if(productRepository.count()>0){
+            return;
+        }
+
+
+        String fileContent = Files.readString(Path.of(RESOURCES_FILE_PATH + PRODUCTS_FILE_NAME));
+
+        ProductSeedDto[] productSeedDtos = gson.fromJson(fileContent, ProductSeedDto[].class);
+
+        Arrays.stream(productSeedDtos)
+                .filter(validationUtil::isValid)
+                .map(productSeedDto -> {
+                    Product product = modelMapper.map(productSeedDto, Product.class);
+                    product.setSeller(userService.findRandomUser());
+                    if(product.getPrice().compareTo(BigDecimal.valueOf(900L)) > 0){
+                        product.setBuyer(userService.findRandomUser());
+                    }
+
+                    product.setCategories(categoryService.findRandomCategories());
+                    return product;
+                }).forEach(productRepository::save);
+
+    }
+
+    @Override
+    public List<ProductNameAndPriceDto> findAllProductsInRangeOrderByPrice(BigDecimal lower, BigDecimal upper) {
+        return productRepository.findAllByPriceBetweenAndAndBuyerIsNullOrderByPriceDesc(lower, upper)
+                .stream()
+                .map(product -> {
+                    ProductNameAndPriceDto productNameAndPriceDto = modelMapper.map(product,ProductNameAndPriceDto.class);
+
+                    productNameAndPriceDto.setSeller(String.format("%s %s",product.getSeller().getFirstName(),
+                            product.getSeller().getLastName()));
+
+
+                    return productNameAndPriceDto;
+                })
+                .collect(Collectors.toList());
+    }
+}
